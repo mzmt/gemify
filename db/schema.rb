a733bde9e44c5a7898a9208a4652c40725b463d1
mzmt <mrcrius1355@gmail.com>
2021-01-11 17:51:54 +0900

    change db schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_10_04_170025) do
 
-  create_table "attends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attends", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "plan_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_170025) do
     t.index ["user_id"], name: "index_attends_on_user_id"
   end
 
-  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "plans", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "user_id", null: false
     t.datetime "start_date", null: false
@@ -35,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_170025) do
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "twitter_id", null: false
     t.string "email"

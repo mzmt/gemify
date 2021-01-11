2747f9fc58fdf11126474300c14cffd3323db285
mzmt <mrcrius1355@gmail.com>
2021-01-11 18:03:55 +0900

    自分のプランのみを表示する

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,8 @@
 class PlansController < ApplicationController
   def index
     @week = Date.today...Date.today.since(7.days)
-    @plans = Plan.where(start_date: Time.current...Time.current.since(7.days)).order(:start_date)
+    @user = User.first
+    @plans = @user.plans.where(start_date: Time.current...Time.current.since(7.days)).order(:start_date)
+    # .where(start_date: date.beginning_of_day...date.end_of_day)
   end
 end

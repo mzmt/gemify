05906c1edab33c7d322fee8594be09cfe05125ec
mzmt <mrcrius1355@gmail.com>
2021-02-03 00:37:11 +0900

    ユーザーページが存在しない場合は/に移動

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,8 +2,10 @@ class PlansController < ApplicationController
   skip_before_action :require_login, raise: false
 
   def show
-    @week = Date.today...Date.today.since(7.days)
     @user = User.find_by(twitter_id: params[:id])
+    return redirect_to '/' unless @user
+
+    @week = Date.today...Date.today.since(7.days)
     @plans = @user.plans.where(start_date: Time.current...Time.current.since(7.days)).order(:start_date)
     # .where(start_date: date.beginning_of_day...date.end_of_day)
   end

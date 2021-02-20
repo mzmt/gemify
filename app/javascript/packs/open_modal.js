e734ad925e85bff731d3d57f53fb1a937478615d
mzmt <mrcrius1355@gmail.com>
2021-02-21 01:29:18 +0900

    日付ごとにモーダルを追加

--- app/javascript/packs/open_modal.js
+++ app/javascript/packs/open_modal.js
@@ -1,18 +1,36 @@
+// open specify modal
 $(function ($) {
   $(document).ready(function () {
-    var open = document.getElementsByClassName('open_modal');
-    $(open).click(function () {
-      var modal = document.getElementById('modal');
-      modal.classList.add('is-active');
-    });
+    // 対象の日付を取得
+    var i;
+    for (i = 0; i < 7; i++) {
+      var today = new Date();
+      var target = new Date(today)
+      target.setDate(target.getDate() + i)
+      var dd = String(target.getDate()).padStart(2, '0');
+      var mm = String(target.getMonth() + 1).padStart(2, '0');
+      var yyyy = target.getFullYear();
+      target = yyyy + '-' + mm + '-' + dd;
+
+      var open = document.getElementById('open-modal-' + target);
+      open.addEventListener('click', openModal, false);
+      open.targetModal = document.getElementById('modal-' + target);
+    }
+
+    function openModal(evt) {
+      evt.currentTarget.targetModal.id = 'activeModal';
+      evt.currentTarget.targetModal.classList.add('is-active');
+    }
   });
 });
 
+// close modal
 $(function ($) {
   $(document).ready(function () {
     $('#close-modal-button' + ',' + '#modal-background').click(function () {
-      var modal = document.getElementById('modal');
+      var modal = document.getElementById('activeModal');
       modal.classList.remove('is-active');
+      modal.removeAttribute('id');
     });
   });
 });

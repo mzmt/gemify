a68da64830173a4e9d9df8f56e396c6507d1a033
mzmt <mrcrius1355@gmail.com>
2021-05-01 19:47:40 +0900

    add OGP and meta tags and Google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'gemify.me',
+      title: 'gemify.me',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'gemify.meでゲームの予定を公開しましょう',
+      keywords: 'game calender',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('logo/icon.png') }
+        # { href: image_url('logo/icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
+      ],
+      og: {
+        site_name: 'gemify.me',
+        title: 'gemify.me',
+        description: @user.name || 'gemify.meでゲームの予定を公開しましょう',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('logo/icon.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary'
+      }
+    }
+  end
 end
